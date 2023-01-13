9d531971beb3dfe8998c5e7624657247f82fc449
vanessa-lanquetin <lanquetin.vanessa@gmail.com>
2023-01-13 13:59:37 +0100

    affichage numero de commande

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -2,3 +2,8 @@ const url = new URL(window.location.href);
 const orderId = url.searchParams.get("orderId");
 
 // Cherecher le span et mettre orderUd dedans si orderId != null
+
+const orderHTML=document.getElementById("orderId");
+if (orderId != null && orderHTML != null) {
+  orderHTML.innerText = orderId;
+}
