c970bca9e3c3756452e1c1202e58bc80c736712f
vanessa-lanquetin <lanquetin.vanessa@gmail.com>
2023-02-22 17:14:58 +0100

    new file form + corrections

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -4,10 +4,11 @@
 const url = new URL(window.location.href);
 const orderId = url.searchParams.get("orderId");
 
-// Cherecher le span et mettre orderId dedans si orderId != null
+
 
 //J'AFFICHE LE NUMERO DE COMMANDE EN AFFICHANT L'ORDERID SUR LA PAGE AVEC LE DOM
 const orderHTML = document.getElementById("orderId");
+// Chercher le span et mettre orderId dedans si orderId != null
 if (orderId != null && orderHTML != null) {
   orderHTML.innerText = orderId;
 }
